da5f82c09174a002b67b434c15e2085cebdf345a
Ayoub EDAHLOULI <79193310+Ayoub-EDAHLOULI@users.noreply.github.com>
2024-03-13 13:10:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,284 @@
+const data = [
+    {
+    "id": 1,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+    "rate": 3.9,
+    "count": 120
+    }
+    },
+    {
+    "id": 2,
+    "title": "Mens Casual Premium Slim Fit T-Shirts ",
+    "price": 22.3,
+    "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    "rating": {
+    "rate": 4.1,
+    "count": 259
+    }
+    },
+    {
+    "id": 3,
+    "title": "Mens Cotton Jacket",
+    "price": 55.99,
+    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    "rating": {
+    "rate": 4.7,
+    "count": 500
+    }
+    },
+    {
+    "id": 4,
+    "title": "Mens Casual Slim Fit",
+    "price": 15.99,
+    "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    "rating": {
+    "rate": 2.1,
+    "count": 430
+    }
+    },
+    {
+    "id": 5,
+    "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    "price": 695,
+    "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    "category": "jewelery",
+    "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    "rating": {
+    "rate": 4.6,
+    "count": 400
+    }
+    },
+    {
+    "id": 6,
+    "title": "Solid Gold Petite Micropave ",
+    "price": 168,
+    "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+    "category": "jewelery",
+    "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    "rating": {
+    "rate": 3.9,
+    "count": 70
+    }
+    },
+    {
+    "id": 7,
+    "title": "White Gold Plated Princess",
+    "price": 9.99,
+    "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+    "category": "jewelery",
+    "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    "rating": {
+    "rate": 3,
+    "count": 400
+    }
+    },
+    {
+    "id": 8,
+    "title": "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    "price": 10.99,
+    "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+    "category": "jewelery",
+    "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    "rating": {
+    "rate": 1.9,
+    "count": 100
+    }
+    },
+    {
+    "id": 9,
+    "title": "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+    "price": 64,
+    "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+    "category": "electronics",
+    "image": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+    "rating": {
+    "rate": 3.3,
+    "count": 203
+    }
+    },
+    {
+    "id": 10,
+    "title": "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+    "price": 109,
+    "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+    "category": "electronics",
+    "image": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+    "rating": {
+    "rate": 2.9,
+    "count": 470
+    }
+    },
+    {
+    "id": 11,
+    "title": "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+    "price": 109,
+    "description": "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
+    "category": "electronics",
+    "image": "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+    "rating": {
+    "rate": 4.8,
+    "count": 319
+    }
+    },
+    {
+    "id": 12,
+    "title": "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    "price": 114,
+    "description": "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
+    "category": "electronics",
+    "image": "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+    "rating": {
+    "rate": 4.8,
+    "count": 400
+    }
+    },
+    ]
+
+
+/*Fetch Data*/
+
+const product = document.querySelector(".product");
+let count = 1 ;
+let counter = document.querySelector("#counter");
+
+
+
+const displayProducts = (items) => {
+
+    for(let item of items){
+        let cards = document.createElement("div");
+        cards.setAttribute('class','card');
+        cards.dataset.id = item.id;
+        let id = cards.dataset.id;
+        cards.innerHTML = `
+                    <img src=${item.image} alt=${item.title}>
+                    <div class="card-body">
+                        <div class="prix">${item.price}</div>
+                        <h1 class="heading-card">${item.title}</h1>
+                        <span></span>
+                        <p class="para-card">${item.description}</p>
+                        <a href="#" class="addBtn">Add To Card</a>
+                    </div>
+        `
+        product.append(cards);
+
+        cards.addEventListener('click', (e) => {
+
+            const navSide = document.querySelector(".navSide");
+            let positionClick = e.target;
+            let Table = []
+
+
+            if(positionClick.classList.contains('addBtn')){
+                let newCard = document.createElement('div');
+                newCard.setAttribute('class','content');
+                newCard.innerHTML = `
+                    <div class="content">
+                    <img src=${item.image} alt=${item.title}>
+                    <span>$${item.price}</span>
+                    <div class="counter">
+                        <button id="decrement">-</button>
+                        <button id="target">1</button>
+                        <button id="increment">+</button>
+                    </div>
+                    </div>
+                `
+                navSide.append(newCard);
+                Table.push(newCard.innerHTML)
+                
+
+                for(let i=0; i<Table.length; i++){
+                    console.log(Table[i])
+                }
+
+
+                let increment = document.querySelectorAll('#increment');
+                let decrement = document.querySelectorAll('#decrement');
+            
+
+                increment.forEach(increaseItems)
+                decrement.forEach(decreaseItems)
+
+
+                function increaseItems(button){
+                    button.addEventListener("click", function (e){
+                    e.target.previousElementSibling.innerText++;
+                    counter.innerHTML++
+                    console.log("hello")
+                    })
+                }
+                    
+
+                function decreaseItems(button) {
+                    button.addEventListener("click", function (e) {
+                    let navSideQuantity = e.target.nextElementSibling.innerText;
+                    if(navSideQuantity > 1){
+                    e.target.nextElementSibling.innerText--;
+                    counter.innerHTML--
+                    }else{
+                        newCard.remove()
+                        counter.innerHTML=0
+                    }
+                    });
+                }
+                    
+            }
+        })
+    }
+}
+
+displayProducts(data);
+
+
+/*Add To Cart Icon*/
+
+const addBtn = document.getElementsByClassName("addBtn");
+
+const incrementProducts = (items) => {
+
+        for(let item of items){
+            item.addEventListener('click', function(){
+                    if(count>0){
+                        console.log("bye")
+                        counter.innerText = count++
+                    }else{
+                        count=0
+                    }
+            }) 
+        }
+
+    return count
+}
+
+incrementProducts(addBtn);
+
+
+
+
+
+/*Show PRODUCT LIST NavSide */
+
+const navSide = document.querySelector(".navSide");
+let shopCart = document.querySelector("#shopCart");
+let closeBtn = document.querySelector('.close'); 
+
+shopCart.addEventListener('click', function(){
+    navSide.classList.toggle('showNav');
+})
+
+closeBtn.addEventListener('click', function(){
+    navSide.classList.toggle('showNav');
+})
+
+
